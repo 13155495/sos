130b1b5312d1f30326791310841b0436807b7cb2
lizi <lizi@163.com>
2018-01-18 18:50:08 +0800

    修改添加好友接口，新增可以通过号码添加好友

--- class/Friend.class.php
+++ class/Friend.class.php
@@ -31,53 +31,55 @@
      * @param  [type] $id [description]
      * @return [type]     [description]
      */
-    public function checkByid($id){
+      public function checkByid($id){
       
-      $sql = "SELECT id FROM user WHERE id ='$id'";
-    
-      //创建一个SqlHelper对象
-      $sqlHelper = new SqlHelper();
-      $arr = $sqlHelper->execute_dql($sql);
-         if(!empty($arr))
-         {
-            return 1;
-         }
-         else
-         {
-            return 0;
-         }
+          $sql = "SELECT id FROM user WHERE id ='$id'";
+        
+          //创建一个SqlHelper对象
+          $sqlHelper = new SqlHelper();
+          $arr = $sqlHelper->execute_dql($sql);
+           if(!empty($arr))
+           {
+              return 1;
+           }
+           else
+           {
+              return 0;
+           }
 
-    }
+      }
+
       /**
-       * [checkRelashion 检测好友记录是否存在]
-       * @param  [type] $email        [description]
-       * @param  [type] $friend_email [description]
-       * @return [type]               [description]
+       * [checkRelashionById 通过用户id和朋友id检测记录是否存在]
+       * @param  [type] $id        [description]
+       * @param  [type] $friend_id [description]
+       * @return [type]            [description]
        */
-      public function checkRelashion($email,$friend_email){
+      public function checkRelashionById($id,$friend_id){
          //2 发起邀请
-         $sql = "SELECT user_id,friend_id,status FROM friend WHERE user_email ='$email' AND friend_email='$friend_email'";
+         $sql = "SELECT user_id,friend_id,status FROM friend WHERE user_id ='$id' AND friend_id='$friend_id'";
       
          //创建一个SqlHelper对象
          $sqlHelper = new SqlHelper();
          $arr = $sqlHelper->execute_dql($sql);
          return $arr[0];
       }
+
       /**
-       * [updateRelashion 更新好友记录状态]
-       * @param  [type] $email        [description]
-       * @param  [type] $friend_email [description]
-       * @param  [type] $status       [description]
-       * @return [type]               [description]
+       * [updateRelashionById 通过ID更新好友记录状态]
+       * @param  [type] $id        [description]
+       * @param  [type] $friend_id [description]
+       * @param  [type] $status    [description]
+       * @return [type]            [description]
        */
-   	  public function updateRelashion($email,$friend_email,$status)
+      public function updateRelashionById($id,$friend_id,$status)
       {
         
          //数据库操作实例化
          $sqlHelper = new SqlHelper();
 
          //////邀请记录--存在更新状态
-         $invite_sql    = "UPDATE friend SET status='$status' WHERE user_email='$email' AND friend_email='$friend_email'";
+         $invite_sql    = "UPDATE friend SET status='$status' WHERE user_id='$id' AND friend_id='$friend_id'";
          //echo $invite_sql;
          $res = $sqlHelper->execute_dml($invite_sql);
          //var_dump($res);
@@ -125,9 +127,24 @@
        * @param  [type] $email [description]
        * @return [type]        [description]
        */
-      public function getUserInfo($email){
+      public function getUserInfoByEmail($email){
          
          $sql = "SELECT id,reg_id,name,email,tel,avatar FROM user WHERE email ='$email'";
+      
+         //创建一个SqlHelper对象
+         $sqlHelper = new SqlHelper();
+         $arr = $sqlHelper->execute_dql($sql);
+         return $arr;
+
+      }
+     /**
+      * [getUserInfoByTel 通过电话获取用户信息]
+      * @param  [type] $tel [description]
+      * @return [type]      [description]
+      */
+      public function getUserInfoByTel($tel){
+         
+         $sql = "SELECT id,reg_id,name,email,tel,avatar FROM user WHERE tel ='$tel'";
       
          //创建一个SqlHelper对象
          $sqlHelper = new SqlHelper();
@@ -156,22 +173,18 @@
          
          //
          $invite_sql = "INSERT INTO friend (
-         user_id,user_name,user_email,user_tel,
-         friend_id,friend_name,friend_email,friend_tel,friend_avatar,
+         user_id,
+         friend_id,
          status,create_time) VALUES 
-         ('".$user_info['id']."','".$user_info['name']."','".$user_info['email']."','".$user_info['tel']."',
-         '".$friend_info['id']."','".$friend_info['name']."','".$friend_info['email']."','".$friend_info['tel']."','".$friend_info['avatar']."',
-         '".$invite_status."','".$create_time."')";
+        ('".$user_info['id']."','".$friend_info['id']."','".$invite_status."','".$create_time."')";
          //
         
 
          $beinvite_sql = "INSERT INTO friend (
-         user_id,user_name,user_email,user_tel,
-         friend_id,friend_name,friend_email,friend_tel,friend_avatar,
+         user_id,
+         friend_id,
          status,create_time) VALUES 
-         ('".$friend_info['id']."','".$friend_info['name']."','".$friend_info['email']."','".$friend_info['tel']."',
-         '".$user_info['id']."','".$user_info['name']."','".$user_info['email']."','".$user_info['tel']."','".$user_info['avatar']."',
-         '".$beinvite_status."','".$create_time."')";
+         ('".$friend_info['id']."','".$user_info['id']."','".$beinvite_status."','".$create_time."')";
          //echo $beinvite_sql;
          //var_dump($invite_sql);var_dump($beinvite_sql);return;
          $sqlHelper = new SqlHelper();
--- friendadd.php
+++ friendadd.php
@@ -9,112 +9,218 @@
 	    
 		require_once 'class/Push.class.php';
 		 
+		
 		 $email 		= getgpc('email', 'G');
 		 $friend_email 	= getgpc('friend_email', 'G');
-   		//用户名密码为空的验证
-		if ($email == ''  || $friend_email == '')
+		 //
+		 $tel 			= getgpc('tel', 'G');
+		 $friend_tel 	= getgpc('friend_tel', 'G');
+   		//验证邮箱地址
+		if (($email != ''  && $friend_email != ''))
 		{
-		
-   			die( JSON(array('res' =>0, 'data' => 'required parameter missing')));	
-		}
-		if($email == $friend_email)
-		{
-			die( JSON(array('res' =>0, 'data' => 'can not add youself')));	
-		}
-		
-		$friend = 	new Friend();
-		
-		
-
-		
-		//先查用户该是否存在
-		$user_info 	 = $friend->getUserInfo($email);
-		$friend_info = $friend->getUserInfo($friend_email);
-
-		//var_dump($friend_info);return;
-		if(empty($user_info[0]))
-		{
-			die(JSON(array ('res'=>0,'data'=>'user info is not exists')));
-		}
-		if(empty($friend_info[0]))
-		{
-			die(JSON(array ('res'=>0,'data'=>'friend info is not exists')));
-		}
-
-		$push   =	new Push();
-		$time 	=   date('Y-m-d H:i:s',time());
-		//推送的信息json --{"msgtype":"receiveadd","fromuid":"2","touid":"1","content":"你有一个好友添加请求","time":12323232323,"lat":"","lng":"","address":"","type":"","isReaded":""} 
- 
-		$msg = JSON(array(
-							'msgtype'=>'receiveadd','fromuid'=>$user_info[0]['id'],'touid'=>$friend_info[0]['id'],
-							'content'=>'你有一个好友添加请求','time'=>$time,'lat'=>'','lng'=>'','address'=>'',
-							'type'=>'','isReaded'=>''
-		));
-		//var_dump($msg);return;
-		//检测email好友关系是否存在
-		$arr1 = $friend->checkRelashion($email,$friend_email);
-		//检测friend_email好友关系是否存在
-		$arr2 = $friend->checkRelashion($friend_email,$email);
-		//var_dump($arr1);var_dump($arr2);return;
-		if(!empty($arr1) &&  !empty($arr2))
-		{
-			//非好友状态才更新
-			if($arr1['status'] != 1 && $arr2['status'] != 1)
+			//不能加自己
+			if($email == $friend_email)
 			{
-				
-				
-				////////////////////推送//////////////////////
-				//var_dump($friend_info[0]['reg_id']);
-				$res = $push->jgPush($friend_info[0]['reg_id'],$msg);
-				if($res == 1)
+				die( JSON(array('res' =>0, 'data' => 'can not add youself')));	
+			}
+
+			//
+			$friend = 	new Friend();	
+			//先查用户该是否存在
+			$user_info 	 = $friend->getUserInfoByEmail($email);
+			$friend_info = $friend->getUserInfoByEmail($friend_email);
+
+			//var_dump($friend_info);return;
+			if(empty($user_info[0]))
+			{
+				die(JSON(array ('res'=>0,'data'=>'user info is not exists')));
+			}
+			if(empty($friend_info[0]))
+			{
+				die(JSON(array ('res'=>0,'data'=>'friend info is not exists')));
+			}
+
+			$push   =	new Push();
+			$time 	=   date('Y-m-d H:i:s',time());
+			//推送的信息json --{"msgtype":"receiveadd","fromuid":"2","touid":"1","content":"你有一个好友添加请求","time":12323232323,"lat":"","lng":"","address":"","type":"","isReaded":""} 
+	 
+			$msg = JSON(array(
+								'msgtype'=>'receiveadd','fromuid'=>$user_info[0]['id'],'touid'=>$friend_info[0]['id'],
+								'content'=>'你有一个好友添加请求','time'=>$time,'lat'=>'','lng'=>'','address'=>'',
+								'type'=>'','isReaded'=>''
+			));
+			//var_dump($msg);return;
+			//检测email好友关系是否存在
+			$arr1 = $friend->checkRelashionById($user_info[0]['id'],$friend_info[0]['id']);
+			//检测friend_email好友关系是否存在
+			$arr2 = $friend->checkRelashionById($friend_info[0]['id'],$user_info[0]['id']);
+			//var_dump($arr1);var_dump($arr2);return;
+			if(!empty($arr1) &&  !empty($arr2))
+			{
+				//非好友状态才更新
+				if($arr1['status'] != 1 && $arr2['status'] != 1)
 				{
-					//更新状态email 向 friend_email 发起邀请 2
-					$res1 = $friend->updateRelashion($email,$friend_email,2);
-					$res2 = $friend->updateRelashion($friend_email,$email,3);
+					
+					
+					////////////////////推送//////////////////////
+					//var_dump($friend_info[0]['reg_id']);
+					$res = $push->jgPush($friend_info[0]['reg_id'],$msg);
+					if($res == 1)
+					{
+						//更新状态email 向 friend_email 发起邀请 2
+						$res1 = $friend->updateRelashionById($user_info[0]['id'],$friend_info[0]['id'],2);
+						$res2 = $friend->updateRelashionById($friend_info[0]['id'],$user_info[0]['id'],3);
+						die(JSON(array ('res'=>1,'data'=>'sucess')));
+					}
+					else
+					{
+						die(JSON(array ('res'=>0,'data'=>'push fail')));
+					}
+					/////////////////////////////////////////////
+					//die(JSON(array ('res'=>1,'data'=>'sucess')));
+
+				}
+				else{
+					die(JSON(array ('res'=>0,'data'=>'already friend')));
+				}
+				
+			}
+			//var_dump($user_info[0]);var_dump($friend_info[0]);return;
+			
+			
+			////////////////////推送//////////////////////
+			//var_dump($friend_info[0]['reg_id']);
+			$res = $push->jgPush($friend_info[0]['reg_id'],$msg);
+			if($res == 1)
+			{
+				//新的好友关系建立
+				$res = $friend->createRelashion($user_info[0],$friend_info[0]);
+				if($res == '1|1')
+				{
 					die(JSON(array ('res'=>1,'data'=>'sucess')));
 				}
-				else
-				{
-					die(JSON(array ('res'=>0,'data'=>'push fail')));
+				else{
+					$res_arr = explode('|',$res); 
+					if($res_arr[0] == '0'){
+						die(JSON(array ('res'=>0,'data'=>'invite fail')));
+					}
+					if($res_arr[1] == '0'){
+						die(JSON(array ('res'=>0,'data'=>'be invite fail')));
+					}
 				}
-				/////////////////////////////////////////////
-				//die(JSON(array ('res'=>1,'data'=>'sucess')));
-
+				
 			}
-			else{
-				die(JSON(array ('res'=>0,'data'=>'already friend')));
-			}
-			
-		}
-		//var_dump($user_info[0]);var_dump($friend_info[0]);return;
-		
-		
-		////////////////////推送//////////////////////
-		//var_dump($friend_info[0]['reg_id']);
-		$res = $push->jgPush($friend_info[0]['reg_id'],$msg);
-		if($res == 1)
-		{
-			//新的好友关系建立
-			$res = $friend->createRelashion($user_info[0],$friend_info[0]);
-			if($res == '1|1')
+			else
 			{
-				die(JSON(array ('res'=>1,'data'=>'sucess')));
+				die(JSON(array ('res'=>0,'data'=>'push fail')));
 			}
-			else{
-				$res_arr = explode('|',$res); 
-				if($res_arr[0] == '0'){
-					die(JSON(array ('res'=>0,'data'=>'invite fail')));
-				}
-				if($res_arr[1] == '0'){
-					die(JSON(array ('res'=>0,'data'=>'be invite fail')));
-				}
-			}
-			
+   			
 		}
-		else
+		
+		//验证电话号码是否为空
+		if (($tel != ''  && $friend_tel != ''))
 		{
-			die(JSON(array ('res'=>0,'data'=>'push fail')));
+			//不能加自己
+			if($tel == $friend_tel)
+			{
+				die( JSON(array('res' =>0, 'data' => 'can not add youself')));	
+			}
+
+			//
+			$friend = 	new Friend();
+			//先查用户该是否存在
+			$user_info 	 = $friend->getUserInfoByTel($tel);
+			$friend_info = $friend->getUserInfoByTel($friend_tel);
+
+			//var_dump($friend_info);return;
+			if(empty($user_info[0]))
+			{
+				die(JSON(array ('res'=>0,'data'=>'user info is not exists')));
+			}
+			if(empty($friend_info[0]))
+			{
+				die(JSON(array ('res'=>0,'data'=>'friend info is not exists')));
+			}
+
+			$push   =	new Push();
+			$time 	=   date('Y-m-d H:i:s',time());
+			//推送的信息json --{"msgtype":"receiveadd","fromuid":"2","touid":"1","content":"你有一个好友添加请求","time":12323232323,"lat":"","lng":"","address":"","type":"","isReaded":""} 
+	 
+			$msg = JSON(array(
+								'msgtype'=>'receiveadd','fromuid'=>$user_info[0]['id'],'touid'=>$friend_info[0]['id'],
+								'content'=>'你有一个好友添加请求','time'=>$time,'lat'=>'','lng'=>'','address'=>'',
+								'type'=>'','isReaded'=>''
+			));
+			//var_dump($msg);return;
+			//检测email好友关系是否存在
+			$arr1 = $friend->checkRelashionById($user_info[0]['id'],$friend_info[0]['id']);
+			//检测friend_email好友关系是否存在
+			$arr2 = $friend->checkRelashionById($friend_info[0]['id'],$user_info[0]['id']);
+			//var_dump($arr1);var_dump($arr2);return;
+			if(!empty($arr1) &&  !empty($arr2))
+			{
+				//非好友状态才更新
+				if($arr1['status'] != 1 && $arr2['status'] != 1)
+				{
+					
+					
+					////////////////////推送//////////////////////
+					//var_dump($friend_info[0]['reg_id']);
+					$res = $push->jgPush($friend_info[0]['reg_id'],$msg);
+					if($res == 1)
+					{
+						//更新状态email 向 friend_email 发起邀请 2
+						$res1 = $friend->updateRelashionById($user_info[0]['id'],$friend_info[0]['id'],2);
+						$res2 = $friend->updateRelashionById($friend_info[0]['id'],$user_info[0]['id'],3);
+						die(JSON(array ('res'=>1,'data'=>'sucess')));
+					}
+					else
+					{
+						die(JSON(array ('res'=>0,'data'=>'push fail')));
+					}
+					/////////////////////////////////////////////
+					//die(JSON(array ('res'=>1,'data'=>'sucess')));
+
+				}
+				else{
+					die(JSON(array ('res'=>0,'data'=>'already friend')));
+				}
+				
+			}
+			//var_dump($user_info[0]);var_dump($friend_info[0]);return;
+			
+			
+			////////////////////推送//////////////////////
+			//var_dump($friend_info[0]['reg_id']);
+			$res = $push->jgPush($friend_info[0]['reg_id'],$msg);
+			if($res == 1)
+			{
+				//新的好友关系建立
+				$res = $friend->createRelashion($user_info[0],$friend_info[0]);
+				if($res == '1|1')
+				{
+					die(JSON(array ('res'=>1,'data'=>'sucess')));
+				}
+				else{
+					$res_arr = explode('|',$res); 
+					if($res_arr[0] == '0'){
+						die(JSON(array ('res'=>0,'data'=>'invite fail')));
+					}
+					if($res_arr[1] == '0'){
+						die(JSON(array ('res'=>0,'data'=>'be invite fail')));
+					}
+				}
+				
+			}
+			else
+			{
+				die(JSON(array ('res'=>0,'data'=>'push fail')));
+			}
+
 		}
+		die( JSON(array('res' =>0, 'data' => 'required parameter missing')));	
+		
+		
 		////////////////////////////////////////////////
 		//die(JSON(array ('res'=>1,'data'=>'sucess')));
 		
